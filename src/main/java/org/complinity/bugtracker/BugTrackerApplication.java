1ae561ad3b31e3a95f97b9cc025daaeba446cea2
k-srivastava <srivastavak1223@gmail.com>
2024-06-12 10:14:09 +0530

    Removed Spring Security default login from app homepage.

--- src/main/java/org/complinity/bugtracker/BugTrackerApplication.java
+++ src/main/java/org/complinity/bugtracker/BugTrackerApplication.java
@@ -2,8 +2,9 @@ package org.complinity.bugtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class BugTrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(BugTrackerApplication.class, args);
