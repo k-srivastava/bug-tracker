e3b20d8c744e49eeba668039f3960c9aa47b271c
k-srivastava <srivastavak1223@gmail.com>
2024-06-26 09:42:56 +0530

    Updated routes for current components.

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import {RouterModule, Routes} from '@angular/router';
-import {AppComponent} from "./app.component";
-import {LoginComponent} from "./components/login/login.component";
 import {NgModule} from "@angular/core";
+
+import {LoginComponent} from "./components/login/login.component";
 import {ProjectCreateComponent} from "./components/project-create/project-create.component";
+import {ProjectComponent} from "./components/project/project.component";
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
-    {path: 'project/create', component: ProjectCreateComponent},
+    {path: 'projects/create', component: ProjectCreateComponent},
+    {path: 'projects/:id', component: ProjectComponent}
 ];
 
 @NgModule({
